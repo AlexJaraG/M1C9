0cb47fb82a7eb56c4e6d1702f7e819be79bf43bd
AlexJaraG <94753704+AlexJaraG@users.noreply.github.com>
2023-09-23 10:55:32 -0600

    agregando botones

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,21 @@
 console.log("Entro index.js");
+
+const btnBlue = document.getElementById('btnBlue');
+
+function clickBtnAzul(){
+    console.log("click en el boton azul");
+}
+
+function overBtnBlue(){
+    console.log("Entro en over");
+}
+
+function changeBackColorPage(color){
+    document.body.style.background = color;
+}
+
+btnBlue.addEventListener("click", () =>{
+    changeBackColorPage("blue");
+});
+
+btnBlue.addEventListener("mouseover", overBtnBlue);
